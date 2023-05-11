0bec5a9187e1aa2265abddae0f7bd8c8b2e80f85
Kirill Trifonov <kirill.tivr@yandex.ru>
2023-05-11 15:14:56 +0300

    проверка2

--- scripts/index.js
+++ scripts/index.js
@@ -6,23 +6,21 @@ let nameInput = document.querySelector('.popup__input_profile_name');
 let jobInput = document.querySelector('.popup__input_profile_job');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
-let savePopupButton = document.querySelector('.popup__button');
+let closePopup = (popupToClose) => popupToClose.classList.toggle('popup_opened');
 
 openPopupButton.addEventListener('click', () => {
-  popup.classList.add('popup__opened'),
+  closePopup(popup)
   nameInput.value = profileName.textContent,
-  jobInput.value = profileAbout.textContent});
+  jobInput.value = profileAbout.textContent
+});
 
-closePopupButton.addEventListener('click', () => 
-  popup.classList.remove('popup__opened'));
+closePopupButton.addEventListener('click', () => closePopup(popup));
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value,
   profileAbout.textContent = jobInput.value;
+  closePopup(popup);
 }
-
-savePopupButton.addEventListener('click', () =>
-  popup.classList.remove('popup__opened'));
 
 formElement.addEventListener('submit', handleFormSubmit);
