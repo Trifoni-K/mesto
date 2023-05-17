190adaff4b01b762fc3381a1ccf5102a0c74b364
Kirill Trifonov <kirill.tivr@yandex.ru>
2023-05-17 15:59:03 +0300

    Проверка4

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// Обьявление переменных
 let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit');
 let closePopupButton = document.querySelector('.popup__close');
@@ -7,29 +8,23 @@ let jobInput = document.querySelector('.popup__input_profile_job');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 
+// Открытие и закрытие попап(редактирование ФИО)
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAbout.textContent;
 }
+openPopupButton.addEventListener('click', openPopup);
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
-
-openPopupButton.addEventListener('click', () => {
-  openPopup();
-  nameInput.value = profileName.textContent,
-  jobInput.value = profileAbout.textContent
-});
-
-closePopupButton.addEventListener('click', () => {
-  closePopup();
-});
+closePopupButton.addEventListener('click', closePopup);
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
-  profileName.textContent = nameInput.value,
+  profileName.textContent = nameInput.value;
   profileAbout.textContent = jobInput.value;
   closePopup();
 }
-
 formElement.addEventListener('submit', handleFormSubmit);
