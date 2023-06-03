d13a331fad2608c45b116104e143db647254b4b5
Kirill Trifonov <kirill.tivr@yandex.ru>
2023-06-04 00:22:10 +0300

    5 спринт 2 проверка

--- scripts/index.js
+++ scripts/index.js
@@ -1,71 +1,63 @@
 // Обьявление переменных popup edit
-const popups = document.querySelectorAll('.popup');
-const popup = Array.from(popups);
-const openPopupButton = document.querySelector('.profile__edit');
-const closePopupButtons = document.querySelectorAll('.popup__close');
-const closePopupButton = Array.from(closePopupButtons);
+const popuplists = document.querySelectorAll('.popup');
+const popuplist = Array.from(popuplists);
+const profileForm = document.querySelector('.popup_type_edit');
+const cardForm = document.querySelector('.popup_type_insert');
+const imageForm = document.querySelector('.popup_type_pic');
+const closeButtonlists = document.querySelectorAll('.popup__close');
+const closeButtonlist = Array.from(closeButtonlists);
 const closePopupEdit = document.querySelector('.popup__close_edit');
-const formPopup = document.querySelectorAll('.popup__form');
+const closePopupInsert = document.querySelector('.popup__close_insert');
+const openPopupButton = document.querySelector('.profile__edit');
 const nameInput = document.querySelector('.popup__input_profile_name');
 const jobInput = document.querySelector('.popup__input_profile_job');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
-const popupEdit = document.querySelector('.popup_type_edit');
-const submitPopup = document.querySelector('.popup__button');
-const submitEdit = document.querySelector('.popup__button_type_edit');
-const popupInsert = document.querySelector('.popup_type_insert');
 const openPopupCards = document.querySelector('.profile__button');
-const closePopupInsert = document.querySelector('.popup__close_insert');
-const submitInsert = document.querySelector('.popup__button_type_insert');
-const popupPic = document.querySelector('.popup_type_pic');
-const cadrsElements = document.querySelector('.elements');
 const templateItem = document.querySelector('.element-template').content;
+const cadrsElements = document.querySelector('.elements');
+const elementCard = document.querySelector('.element');
+const cardName = document.querySelector('.element__title');
 const nameCardInput = document.querySelector('.popup__input_card_name');
 const linkCardInput = document.querySelector('.popup__input_card_link');
-const cardName = document.querySelector('.element__title');
-const cardLink = document.querySelector('.element__image');
 const picCard = document.querySelector('.popup__pic');
 const picLink = document.querySelector('.popup__image');
 const picName = document.querySelector('.popup__about');
-const elementCard = document.querySelector('.element');
 const popupInput = document.querySelector('.popup__input');
 
 // Функции открытия и закрытия попап
-const openPopup = (popup) => {
-  popup.classList.add('popup_opened');
+const openPopup = (popuplist) => {
+  popuplist.classList.add('popup_opened');
 };
-
 openPopupButton.addEventListener('click', () => {
   nameInput.value = profileName.textContent;
   jobInput.value = profileAbout.textContent;
-  openPopup(popupEdit);
+  openPopup(profileForm);
 });
 
-const closePopup = (popup) => {
-  popup.classList.remove('popup_opened');
+const closePopup = (popuplist) => {
+  popuplist.classList.remove('popup_opened');
 };
-
-closePopupButtons.forEach((item) => {
+closeButtonlists.forEach((item) => {
   item.addEventListener('click', () => {
-    popups.forEach((item) => {
-      closePopup(item);
-    })
-  })  
+    popuplists.forEach(closePopup);
+  });
 });
 
+// Профиль
 const handleFormSubmitEdit = (evt) =>{
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = jobInput.value;
-  closePopup(popupEdit);
+  closePopup(profileForm);
 };
-popupEdit.addEventListener('submit', handleFormSubmitEdit);
-
-openPopupCards.addEventListener('click', () => {
-  openPopup(popupInsert);
-});
+profileForm.addEventListener('submit', handleFormSubmitEdit);
 
 // Карточки
+openPopupCards.addEventListener('click', () => {
+  openPopup(cardForm);
+});
+
 const initialCards = [
   {
     name: 'Архыз',
@@ -97,24 +89,26 @@ const deleteItem = (evt) => {
   const item = evt.target.closest('.element');
   item.remove();
 };
-const likeItem = (evt) => {
-  const item = evt.target.classList.toggle('element__button_active');
+const toggleLike = (evt) => {
+  evt.target.classList.toggle('element__button_active');
 };
 const showItem = (evt) => {
-  openPopup(popupPic);
-  picLink.src = evt.target.closest('.element__image').src;
+  openPopup(imageForm);
+  const cardLink = evt.target.closest('.element__image');
+  picLink.src = cardLink.src;
+  picLink.alt = cardLink.alt;
   picName.textContent = evt.target.closest('.element').textContent;
-  picLink.alt = evt.target.closest('.element__image').alt;
 };
 
 const renderCard = (link, name) => {
   const itemElement = templateItem.cloneNode(true);
-  itemElement.querySelector('.element__image').src = link;
-  itemElement.querySelector('.element__image').alt = name;
+  const cardLink = itemElement.querySelector('.element__image');
+  cardLink.src = link;
+  cardLink.alt = name;
   itemElement.querySelector('.element__title').textContent = name;
   itemElement.querySelector('.element__delete').addEventListener('click', deleteItem);
-  itemElement.querySelector('.element__button').addEventListener('click', likeItem);
-  itemElement.querySelector('.element__image').addEventListener('click', showItem);
+  itemElement.querySelector('.element__button').addEventListener('click', toggleLike);
+  cardLink.addEventListener('click', showItem);
   return itemElement;
 };
 
@@ -128,8 +122,7 @@ const handleFormSubmitInsert = (evt) => {
   evt.preventDefault();
   const newCard = renderCard(linkCardInput.value, nameCardInput.value);
   addCard(newCard);
-  linkCardInput.value = '';
-  nameCardInput.value = '';
-  closePopup(popupInsert);
+  evt.target.reset();
+  closePopup(cardForm);
 };
-popupInsert.addEventListener('submit', handleFormSubmitInsert);
+cardForm.addEventListener('submit', handleFormSubmitInsert);
